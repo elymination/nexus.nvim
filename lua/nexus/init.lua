1b7dc8fd9ba92f493b1551a96f8bae55afe897ce
elymination <axel.dubillet@gmail.com>
2023-12-26 19:43:11 -0500

    Added vertical and horizontal split mode (v and h keymap respectively).

--- lua/nexus/init.lua
+++ lua/nexus/init.lua
@@ -1,10 +1,17 @@
 local popup = require("plenary.popup")
 local M = {}
 
+local NEXUS_WIN_TITLE = "Nexus"
+
+local MODE_NORMAL = 0
+local MODE_VERTICAL = 1
+local MODE_HORIZONTAL = 2
+
 M.associations = {}
 M.popup_content = {}
 M.popup_size = 0
 M.popup_winid = nil
+M.popup_mode = MODE_NORMAL
 
 local function create_window(size)
   local width = 60
@@ -13,7 +20,7 @@ local function create_window(size)
   local bufnr = vim.api.nvim_create_buf(false, true)
 
   local win_id, win = popup.create(bufnr, {
-    title = "Nexus",
+    title = NEXUS_WIN_TITLE,
     highlight = "NexusWindow",
     line = math.floor(((vim.o.lines - height) / 2) - 1),
     col = math.floor((vim.o.columns - width) / 2),
@@ -40,6 +47,7 @@ function M.close_window()
     M.popup_winid = nil
     M.popup_content = {}
     M.popup_size = 0
+    M.popup_mode = MODE_NORMAL
   end
 end
 
@@ -110,24 +118,51 @@ end
 function M.select_menu_item()
   local idx = vim.fn.line(".")
   local buf = get_or_create_buffer(M.popup_content[idx])
+  local mode = M.popup_mode
   M.close_window()
   if buf ~= nil then
+    if mode == MODE_VERTICAL then
+      vim.cmd("vs")
+    elseif mode == MODE_HORIZONTAL then
+      vim.cmd("sp")
+    end
     vim.api.nvim_set_current_buf(buf)
     vim.api.nvim_buf_set_option(buf, "buflisted", true)
   end
 end
 
 function M.select_menu_item_idx(idx)
-  if (idx > M.popup_size) then
+  if idx > M.popup_size then
     return
   end
   local buf = get_or_create_buffer(M.popup_content[idx])
-  vim.api.nvim_win_close(M.popup_winid, true)
+  local mode = M.popup_mode
+  M.close_window()
   if buf ~= nil then
+    if mode == MODE_VERTICAL then
+      vim.cmd("vs")
+    elseif mode == MODE_HORIZONTAL then
+      vim.cmd("sp")
+    end
     vim.api.nvim_set_current_buf(buf)
     vim.api.nvim_buf_set_option(buf, "buflisted", true)
   end
-  M.popup_winid = nil
+end
+
+function M.set_mode(mode)
+  if M.popup_winid ~= nil then
+    M.popup_mode = mode
+  end
+end
+
+function M.toggle_mode(mode)
+  if M.popup_winid ~= nil then
+    if M.popup_mode ~= mode then
+      M.set_mode(mode)
+    else
+      M.set_mode(MODE_NORMAL)
+    end
+  end
 end
 
 function M.toggle()
@@ -167,6 +202,12 @@ function M.toggle()
     vim.api.nvim_buf_set_keymap(bufnr, "n", "8", "<Cmd>lua require('nexus').select_menu_item_idx(8)<CR>", {})
     vim.api.nvim_buf_set_keymap(bufnr, "n", "9", "<Cmd>lua require('nexus').select_menu_item_idx(9)<CR>", {})
     vim.api.nvim_buf_set_keymap(bufnr, "n", "0", "<Cmd>lua require('nexus').select_menu_item_idx(10)<CR>", {})
+
+    -- mode toggling
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "n", "<Cmd>lua require('nexus').set_mode(0)<CR>", {}) -- normal
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "v", "<Cmd>lua require('nexus').toggle_mode(1)<CR>", {}) -- vertical
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "h", "<Cmd>lua require('nexus').toggle_mode(2)<CR>", {}) -- horizontal
+
   elseif size == 1 then -- only one associated file, open it
     local buf = get_or_create_buffer(associated_files[1])
     if buf ~= nil then
